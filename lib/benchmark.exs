33be16bfc9661214fbec45b4f3cadf33d0ebb04c
Joseph Martin Giralt <joe.m.giralt@gmail.com>
2021-12-12 01:29:12 -0500

    add day 11 part 2

--- lib/benchmark.exs
+++ lib/benchmark.exs
@@ -22,8 +22,8 @@ Benchee.run(
     "Day 9, Part 2" => fn -> Aoc2021.Day9.Solution.part(2, :over_a_second) end,
     "Day 9, Part 2" => fn -> Aoc2021.Day9.Solution.part(2, :graph) end,
     "Day 10, Part 1" => fn -> Aoc2021.Day10.Solution.part(1) end,
-    "Day 11, Part 1" => fn -> Aoc2021.Day11.Solution.part(1) end
-
+    "Day 11, Part 1" => fn -> Aoc2021.Day11.Solution.part(1) end,
+    "Day 11, Part 2" => fn -> Aoc2021.Day11.Solution.part(2) end
   },
   warmup: 0.2,
   time: 0.2,
--- lib/day11/solution.ex
+++ lib/day11/solution.ex
@@ -1,42 +1,67 @@
 defmodule Aoc2021.Day11.Solution do
   require IEx
 
-  alias Aoc2021.Utils.Matrix, as: M
+  # alias Aoc2021.Utils.Matrix, as: M
 
   def part(1) do
-    lines = input()
-
-    grid =
-      for {line, row} <- Enum.with_index(lines),
-          {number, col} <- Enum.with_index(String.to_charlist(line)),
-          into: %{} do
-        {{row, col}, number - ?0}
-      end
-
-    start_flashing(grid, 100, [])
+    input()
+    |> grid_from_lines()
+    |> start_flashing(100, [])
     |> Enum.map(&MapSet.size(&1))
     |> Enum.sum()
   end
 
   def part(2) do
     input()
+    |> grid_from_lines()
+    |> find_iteration_for_uniflash(0, 0)
+  end
+
+  def grid_from_lines(lines) do
+    for {line, row} <- Enum.with_index(lines),
+        {number, col} <- Enum.with_index(String.to_charlist(line)),
+        into: %{} do
+      {{row, col}, number - ?0}
+    end
+  end
+
+  def find_iteration_for_uniflash(grid, iteration, count) when is_map(grid) do
+    if count == 1 do
+      iteration
+    else
+      [_, reduced_grid] =
+        grid
+        |> Map.new(fn {point, v} -> {point, v + 1} end)
+        |> reduce_to_flash()
+
+      count =
+        reduced_grid
+        |> Map.values()
+        |> Enum.uniq()
+        |> Enum.count()
+
+      find_iteration_for_uniflash(reduced_grid, iteration + 1, count)
+    end
   end
 
   def start_flashing(_, 0, flashes), do: flashes
 
   def start_flashing(grid, iteration, flashes) do
     # IO.inspect(M.list_matrix_from_grid(grid, 10, 10) |> M.unwrap(), label: iteration)
+    [flash_points_set, reduced_grid] =
+      grid
+      |> Map.new(fn {point, v} -> {point, v + 1} end)
+      |> reduce_to_flash()
 
-    grid = Map.new(grid, fn {point, v} -> {point, v + 1} end)
+    start_flashing(reduced_grid, iteration - 1, [flash_points_set | flashes])
+  end
 
-    [reduces_set, reduced_grid] =
-      Enum.reduce(grid, [MapSet.new(), grid], fn {point, _}, acc ->
-        [acc_set, acc_grid] = acc
-        {new_flashes, flashed_grid} = flash({acc_set, acc_grid}, point)
-        [MapSet.union(acc_set, new_flashes), flashed_grid]
-      end)
-
-    start_flashing(reduced_grid, iteration - 1, [reduces_set | flashes])
+  def reduce_to_flash(grid) do
+    grid
+    |> Enum.reduce([MapSet.new(), grid], fn {point, _}, [acc_set, acc_grid] = _acc ->
+      {new_flashes, flashed_grid} = flash({acc_set, acc_grid}, point)
+      [MapSet.union(acc_set, new_flashes), flashed_grid]
+    end)
   end
 
   def flash({set, grid} = _tup, {row, col} = point) when row in 0..9 and col in 0..9 do
